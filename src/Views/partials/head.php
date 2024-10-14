d4d31d25c45880e1ba5977d09e0c688ffb74700d
ayou985 <chelhayoub6@gmail.com>
2024-10-14 15:19:58 +0200

    création du modele pokemon

--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -17,7 +17,7 @@
     <nav class="navbar navbar-expand-lg myNav">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="/public/img/logo.png" alt="Logo" width="50" height="50"> Pokemon
+                <img src="/public/img/logo-Pokemon.jpg" alt="Logo" width="175" height="115"> Pokemon
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
